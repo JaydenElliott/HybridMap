b7920f0b9305576a1d706b7b46426cbdff707a94
JaydenElliott <jayden.elliott@outlook.com>
2022-01-02 11:01:38 +1100

    Insert  function.

--- src/hybrid_map.cpp
+++ src/hybrid_map.cpp
@@ -1,27 +1,30 @@
 #include <vector>
 #include "hash.cpp"
 
-template <typename T, size_t S>
+/// T: Hash value type 
+/// S: Initial hashmap capacity
+/// F: Hash function 
+/// I: Hash functin input type
+template <typename T, size_t S, typename F, typename I>
 struct HybridMap {
-  // std::vector<Hash<int>> map;
+
   public:
-    Hash<T> m_map[S];
     constexpr int Size() const {
       return S;
     } 
 
   private:
-    // Pointer to the hashmap in memory
-    Hash<T> *m_data = nullptr;
+    /// Pointer to the hashmap in memory
+    Hash<T> *m_data[S];
 
-    // Number of elements in the hashmap
+    /// Number of elements in the hashmap
     size_t m_size = 0;
 
-    // Size of allocated block of memory
-    size_t m_capacity = 0;
+    /// Size of allocated block of memory
+    size_t m_capacity = S;
 
 
-    // Reallocates the hashmap with the size new_size
+    /// Reallocates the hashmap with the size new_size
     void realloc(const size_t new_size) {
       Hash<T>* new_data = new T[new_size];
 
@@ -36,8 +39,17 @@ struct HybridMap {
       m_capacity = new_size;
     }
 
+
     Hash<T> operator[](const size_t idx) const {
+      // add bounds check
       return m_data[idx]; 
     } 
+
+    /// input: hash function input
+    /// value: value to insert
+    void insert(const I input, const T value){
+      // bounds chec
+      m_data[F(input)] = value;
+    }
 };
 
